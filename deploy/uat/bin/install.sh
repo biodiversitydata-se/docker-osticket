d729744b36e92768cbadb3ef9c3b5692521f2461
Hans Höök <hans.hook@nrm.se>
2021-11-09 15:11:22 +0100

    Deployment scripts and configuration

---
+++ deploy/uat/bin/install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+cd $(dirname $0)
+. utils.sh
+cd ..
+
+[ ! -e ./deployment.cnf] &&  log_fatal 90 "./deployment.cnf file not found"
+
+export $(grep -v '^#' ./deployment.cnf | xargs)
+
+[ -z "$DEPLOYMENT_HOST" ] &&  log_fatal 91 "DEPLOYMENT_HOST not in ./deployment.cnf file" 
+[ -z "$APPLICATION_SETTINGS_DIR" ] &&  log_fatal 92 "APPLICATION_SETTTINGS_DIR not in ./deployment.cnf file" 
+[ -z "$APPLICATION_NAME" ] &&  log_fatal 93 "APPLICATION_NAME not in ./deployment.cnf file" 
+
+
