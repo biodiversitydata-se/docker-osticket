3887abaab5aa75d2ec83e06253ad04e39d6f461a
Hans Höök <hans.hook@nrm.se>
2021-11-12 09:14:32 +0100

    New deployment structure

--- deploy/uat/bin/install.sh
+++ deploy/uat/bin/install.sh
@@ -9,11 +9,24 @@ cd ..
 export $(grep -v '^#' ./deployment.cnf | xargs)
 
 [ -z "$DEPLOYMENT_HOST" ] &&  log_fatal 91 "DEPLOYMENT_HOST not in ./deployment.cnf file" 
-[ -z "$APPLICATION_SETTINGS_DIR" ] &&  log_fatal 92 "APPLICATION_SETTTINGS_DIR not in ./deployment.cnf file" 
-[ -z "$APPLICATION_NAME" ] &&  log_fatal 93 "APPLICATION_NAME not in ./deployment.cnf file" 
+[ -z "$DEPLOYMENT_USER" ] &&  log_fatal 92 "DEPLOYMENT_USER not in ./deployment.cnf file" 
+[ -z "$APPLICATION_DOCKER_DIR" ] &&  log_fatal 93 "APPLICATION_DOCKER_DIR not in ./deployment.cnf file" 
+[ -z "$APPLICATION_NAME" ] &&  log_fatal 94 "APPLICATION_NAME not in ./deployment.cnf file" 
 
-cd settings
+cd etc
 
-[ ! -d $APPLICATION_NAME ] &&  log_fatal 94 "$APPLICATION_NAME is not a direcotry in $(pwd)"
+[ ! -d $APPLICATION_NAME ] &&  log_fatal 95 "$APPLICATION_NAME is not a direcotry in $(pwd)"
 
-echo "rsync -azP --delete ${APPLICATION_NAME} ${DEPLOYMENT_HOST}:${APPLICATION_SETTINGS_DIR}"
+rsync -azP --delete --exclude '\.git*' ${APPLICATION_NAME} ${DEPLOYMENT_USER}@${DEPLOYMENT_HOST}:${APPLICATION_DOCKER_DIR}/etc
+
+
+cd ..
+if [ -d usr ]
+then
+    cd usr
+    [ ! -d $APPLICATION_NAME ] &&  log_fatal 95 "$APPLICATION_NAME is not a direcotry in $(pwd)"
+    rsync -azP --delete --exclude '\.git*' ${APPLICATION_NAME} ${DEPLOYMENT_USER}@${DEPLOYMENT_HOST}:${APPLICATION_DOCKER_DIR}/usr
+fi
+
+    
+    
